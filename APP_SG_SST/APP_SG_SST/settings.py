f7057baa8b8f4cd88cf0a695b3f98a9abe2ad71f
GLOOMYY <sebas.mesa.montoya@gmail.com>
2021-10-27 06:58:39 -0500

    Commit base de datos - Sebastian

--- APP_SG_SST/APP_SG_SST/settings.py
+++ APP_SG_SST/APP_SG_SST/settings.py
@@ -84,6 +84,12 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+# {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
